8fc4785896b409475b625e5300980939f39e3f1c
pihh <filipemotasa@hotmail.com>
2025-04-24 02:44:37 +0100

    x

--- main.js
+++ main.js
@@ -34,7 +34,8 @@ app.post("/offer", async (req, res) => {
 
 // === /accept ===
 // PC sends answer here
-let androidToken = "ANDROID_FCM_TOKEN";
+let androidToken =
+  "eGS1UQKAap-4mtpapr1f36:APA91bG0wAQzGcNylE-t_Rt7D00iMJXK8xu9LN2V-eVTI-4SwahPbYIs1P3_oqIkZwv05dNCGSXsDp3EMlH4LMVlV-faTU4_7s-fdepQ82obd4mwHLZ6PhY";
 
 // Endpoint to accept the answer from the PC
 app.post("/accept", (req, res) => {
