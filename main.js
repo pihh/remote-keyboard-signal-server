86b8c4da262e4123dbf492db3f7dce10c5c85343
pihh <filipemotasa@hotmail.com>
2025-04-24 01:55:46 +0100

    xx

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(cors());
+app.options("*", cors()); // <- this line
 // Initialize Firebase Admin SDK
 
 // === /offer ===
